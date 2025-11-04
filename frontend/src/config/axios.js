a2b1cf383e3c625cb81ba3cf122582a447cd459b
Ashi Gupta <ashi2004gupta@gmail.com>
2025-11-05 03:22:52 +0530

    Create Project route on frontend and Home.jsx

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -4,6 +4,9 @@ import axios from 'axios'
 
  const axiosInstance=axios.create({
     baseURL:import.meta.env.VITE_API_URL,
+    headers:{
+      "Authorization": `Bearer ${localStorage.getItem('token')}`  //attach token to every request sent from frontend to backend  
+    }
  }
 
  )
